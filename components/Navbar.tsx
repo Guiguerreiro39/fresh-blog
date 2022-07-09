bef167d2ac115934d392de04eea10d940a04b468
Guilherme <guiguerreiro10@hotmail.com>
2022-07-09 23:49:52 +0100

    title and favicon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,9 @@ export default function Navbar(props: NavbarProps) {
     <nav
       class={tw`top-0 w-full h-20 px-36 bg-gray-900 absolute flex items-center gap-10`}
     >
-      <a href="/" class={tw`text-white border-none my-0 mr-10 text-2xl hover:text-red-400`}>GG's Blog</a>
+      <a href="/" class={tw`text-white border-none my-0 mr-10 text-2xl hover:text-red-400`}>
+        <img src="title.png" alt="GG'Blog" class={tw`h-8 w-auto`}/>
+      </a>
       {props.sections.map((section) => (
         <a href={`/${section}`} class={tw`text-white my-0 capitalize text-lg hover:text-red-400`}>
           {section}
